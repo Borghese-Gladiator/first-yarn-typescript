ef7ee2af456426e988547205e5b8ede345834e51
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2021-03-27 00:54:25 -0400

    fix listen to work with Heroku (or any production server)

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -3,7 +3,7 @@ import cors from "cors";
 import express from "express";
 import { join } from "path";
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 app.use(cors());
